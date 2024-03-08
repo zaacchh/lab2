563824fdb0cdfb47844df4fe68ce56a975f235f4
zdsujide <156282110+zdsujide@users.noreply.github.com>
2024-03-08 10:51:57 +0800

    Update index.js
    Incorporation of Lesson 3: Booleans

--- week5/scripts/index.js
+++ week5/scripts/index.js
@@ -7,7 +7,13 @@ const card = document.querySelector(".card");
 // Lesson 2: Variables, Let, Const & Data Types
 // Using let to declare a variable targetSection and getting the data-section attribute.
 let targetSection = e.target.getAttribute("data-section");
-  
+
+// Lesson 3: Booleans
+// Using a ternary operator to check if targetSection is not equal to "#about".
+// If true, add "is-active" class to card, else remove it.
+targetSection !== "#about" ?
+  card.classList.add("is-active") :
+  card.classList.remove("is-active");
   
 // Lesson 2: Variables, Let, Const & Data Types
 // Using const to declare a variable section and selecting the target section.
