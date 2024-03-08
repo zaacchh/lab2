37a2c6895c3fd6be76aa8f456bcdd94d23ff490e
zdsujide <156282110+zdsujide@users.noreply.github.com>
2024-03-09 00:13:46 +0800

    Add files via upload

--- week8/index.php
+++ week8/index.php
@@ -1,29 +1,39 @@
-<!DOCTYPE html>
-<html>
-<body>
- 
 <?php
-echo "My first PHP script!";
+// Lesson 2: Variables
+// Declare variables to store profile information
+$name = "Zach Luis Sujide";
+$age = 22;
+$jobTitle = "Asia Pacific College Student";
+
+// Lesson 3: Echo / Print
+// Print profile information
+echo "<h1>$name</h1>";
+echo "<h2>$jobTitle</h2>";
+
+// Lesson 4: Data Types
+// Use different data types
+$isStudent = true;
+$skills = array("HTML", "CSS", "JavaScript", "PHP", "Dart");
+
+// Lesson 5: Operators & Math
+// Perform a simple calculation
+$birthYear = 2002;
+$currentYear = date("Y");
+$age = $currentYear - $birthYear;
+echo "Age: " . $age;
+
+// Lesson 6: Conditionals
+// Display content based on age
+if ($age >= 18) {
+    echo "You are an adult.";
+} else {
+    echo "You are a minor.";
+}
+
+// Lesson 7: Loops
+// Display hobbies using a loop
+$hobbies = array("Playing", "Competing in PVP games", "Exploring new technologies");
+foreach ($hobbies as $hobby) {
+    echo "<p>$hobby</p>";
+}
 ?>
-
-
-<?php
-ECHO "Hello World!<br>";
-echo "Hello World!<br>";
-EcHo "Hello World!<br>";
-
-$x = "Hello world!";
-$y = 'Hello world!';
-
-var_dump($x);
-echo "<br>";
-var_dump($y);
-
-?>
-
-
-
-
-
-</body>
-</html>
